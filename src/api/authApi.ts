6273ec3433e05efc77d723ec645208dff4646db6
kida21 <kidusp22@gmail.com>
2025-12-24 09:33:26 +0300

    authApi added

---
+++ src/api/authApi.ts
@@ -0,0 +1,11 @@
+
+import axios from 'axios';
+
+const authApi = axios.create({
+  baseURL: import.meta.env.VITE_API_URL_AUTH,
+  withCredentials: true, 
+});
+
+export const login = (email: string, password: string) =>
+  authApi.post('/login', { email, password });
+
